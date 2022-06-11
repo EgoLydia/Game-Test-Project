c1cde8a597ec78fda652e6d91f283d3d94d03f33
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-06-11 01:42:40 +0100

    Added eventlisteners to show and hide sections

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 "use strict";
+const loginBtn = document.querySelector(".login-btn");
+const categoryPage = document.querySelector(".category-page");
+const loginPage = document.querySelector(".login-page");
+const startBtn = document.querySelector(".start-btn");
+const contentPage = document.querySelector(".content-page");
+const nextBtn = document.querySelector(".next-btn");
+const resultPage = document.querySelector(".result-page");
+const exitLeaderPage = document.querySelector(".exit-btn");
+const openLeaderPage = document.querySelector(".leaderBoard-btn");
+const leaderBoard = document.querySelector(".leader-board-page ");
+
+loginBtn.addEventListener("click", openTriviaSetup);
+startBtn.addEventListener("click", openTrivia);
+nextBtn.addEventListener("click", finish);
+exitLeaderPage.addEventListener("click", exitLeaderBoard);
+openLeaderPage.addEventListener("click", openLeaderBoard);
+
+function openTriviaSetup() {
+  categoryPage.classList.remove("hidden");
+  loginPage.classList.add("hidden");
+}
+
+function openTrivia() {
+  categoryPage.classList.add("hidden");
+  contentPage.classList.remove("hidden");
+}
+
+function finish() {
+  contentPage.classList.add("hidden");
+  resultPage.classList.remove("hidden");
+}
+
+function exitLeaderBoard() {
+  leaderBoard.classList.add("hidden");
+  resultPage.classList.remove("hidden");
+}
+
+function openLeaderBoard() {
+  leaderBoard.classList.remove("hidden");
+  resultPage.classList.add("hidden");
+}
