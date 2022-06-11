18c096cd6cfd25a690787fd83be44cc726aa559f
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-06-11 14:31:42 +0100

    Added username authentication

--- script.js
+++ script.js
@@ -9,12 +9,33 @@ const resultPage = document.querySelector(".result-page");
 const exitLeaderPage = document.querySelector(".exit-btn");
 const openLeaderPage = document.querySelector(".leaderBoard-btn");
 const leaderBoard = document.querySelector(".leader-board-page ");
+let userNameInput = document.querySelector(".username");
 
-loginBtn.addEventListener("click", openTriviaSetup);
+loginBtn.addEventListener("click", login);
 startBtn.addEventListener("click", openTrivia);
 nextBtn.addEventListener("click", finish);
 exitLeaderPage.addEventListener("click", exitLeaderBoard);
 openLeaderPage.addEventListener("click", openLeaderBoard);
+
+let currentUser = undefined;
+function login() {
+  const userName = userNameInput.value.toLowerCase();
+  if (userName === "") {
+    alert("Input Name");
+    return;
+  }
+  let user = JSON.parse(localStorage.getItem(userName)) || undefined;
+  console.log(user);
+  if (user === undefined) {
+    user = {
+      name: userName,
+    };
+    console.log(JSON.stringify(user));
+    localStorage.setItem(userName, JSON.stringify(user));
+  }
+  currentUser = user;
+  openTriviaSetup();
+}
 
 function openTriviaSetup() {
   categoryPage.classList.remove("hidden");
