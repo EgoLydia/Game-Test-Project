148540c53475f76c7583e568c3ef3b7d20379a8a
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-06-17 11:40:40 +0100

    code updated with comments

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 "use strict";
 
+// get all the elements
 const loginBtn = document.querySelector(".login-btn");
 const categoryPage = document.querySelector(".category-page");
 const loginPage = document.querySelector(".login-page");
 const startBtn = document.querySelector(".start-btn");
 const contentPage = document.querySelector(".content-page");
 const rule = document.querySelector(".rule");
-let nextBtn = document.querySelector(".next-btn");
 const resultPage = document.querySelector(".result-page");
 const exitLeaderPage = document.querySelector(".exit-btn");
 const openLeaderPage = document.querySelector(".leaderBoard-btn");
 const leaderBoardPage = document.querySelector(".leader-board-page ");
+let nextBtn = document.querySelector(".next-btn");
 let userNameInput = document.querySelector(".username");
 let categoryDisplay = document.querySelector(".categories");
-let spinner = document.querySelector(".spin1");
-let spinner2 = document.querySelector(".spin2");
+const spinner = document.querySelector(".spin1");
+const spinner2 = document.querySelector(".spin2");
 let questNumber = document.querySelector(".quest-number");
 let question = document.querySelector(".question");
 let allOptions = document.querySelectorAll(".answer");
@@ -26,6 +27,7 @@ let playAgainBtn = document.querySelector(".play-again");
 let goHomeBtn = document.querySelector(".go-back");
 let progressBar = document.querySelector("#progress");
 
+// added eventlisteners to selected buttons
 loginBtn.addEventListener("click", login);
 startBtn.addEventListener("click", startTrivia);
 nextBtn.addEventListener("click", submit);
@@ -34,24 +36,28 @@ openLeaderPage.addEventListener("click", viewLeaderBoard);
 playAgainBtn.addEventListener("click", playAgain);
 goHomeBtn.addEventListener("click", goBackHome);
 
+// set global variables to be used in the program
 let currentUser = undefined;
 let selectedLevel = "";
 let selectedCategory = {};
 let questions = [];
-let categories = [];
+let categories = []; //all the categories
 let count = 0;
 let currentQuestion = {};
 let selectedOption = "";
 let currentScore = 0;
 
 function login() {
+  //set input of username
   let userName = userNameInput.value.toLowerCase();
 
   if (userName === "") {
     alert("Please enter a name");
     return;
   }
+  //store user into username
   let user = JSON.parse(localStorage.getItem(userName)) || undefined;
+  // new user created
   if (user === undefined) {
     user = {
       name: userName,
@@ -65,6 +71,7 @@ function login() {
   openTriviaSetup();
 }
 
+// add check event to select levels
 let checkLevel = document.querySelectorAll(".levels");
 for (let i = 0; i < checkLevel.length; i++) {
   checkLevel[i].onchange = (e) => {
@@ -84,11 +91,13 @@ function startTrivia() {
 }
 
 async function fetchCategories() {
+  //fetch frpm api url
   const response = await fetch("https://opentdb.com/api_category.php");
   const result = await response.json();
   if (result) {
     spinner.classList.remove("spinner-border");
   }
+  //loop and set innerhtml of the category class
   categories = result.trivia_categories;
   categories.forEach((trivia_categories) => {
     categoryDisplay.innerHTML += `
@@ -109,6 +118,7 @@ async function fetchCategories() {
 }
 
 function addEventToCategories() {
+  //add check event to selecting category
   let checkCategory = document.querySelectorAll(".category");
   for (let i = 0; i < checkCategory.length; i++) {
     checkCategory[i].onchange = (e) => {
@@ -138,6 +148,7 @@ async function fetchQuestions() {
     spinner2.classList.remove("spinner-border");
   }
   questions = res.results;
+  // check that the questions in the category and level is greater than 0
   if (questions.length > 0) {
     openTrivia();
     nextQuestion();
@@ -160,6 +171,7 @@ function selectOption() {
 selectOption();
 
 function submitAnswer() {
+  // check selected option is the right answer
   if (currentQuestion.correct_answer === selectedOption) currentScore++;
   userScore.innerText = `Score: ${currentScore}`;
   selectedOption = "";
@@ -171,10 +183,12 @@ function nextQuestion() {
   currentQuestion = questions[count];
   let questionLength = questions.length;
   let options = [...currentQuestion.incorrect_answers];
+  // randomly place correct and incorrect answers at different positions
   let index = Math.floor(Math.random() * 4);
   options.splice(index, 0, currentQuestion.correct_answer);
   questNumber.innerHTML = ` Question ${count + 1}/${questionLength}`;
   question.innerHTML = `${currentQuestion.question.toUpperCase()}`;
+  // display options
   for (let i = 0; i < options.length; i++) {
     allOptions[i].children[1].innerText = options[i];
   }
@@ -192,10 +206,12 @@ function submit() {
     alert("Please select an option");
     return;
   }
+  // return next checked option unchecked
   for (let i = 0; i < uncheckOption.length; i++) {
     uncheckOption[i].checked = false;
   }
   submitAnswer();
+
   if (count === questions.length) {
     setHighestScore();
     finish();
@@ -206,12 +222,13 @@ function submit() {
   nextQuestion();
   if (count === questions.length) {
     nextBtn.innerText = `Finish`;
-    console.log(nextBtn.innerText);
   }
 }
 
 function setHighestScore() {
+  // initialize highest score
   let highestScore = 0;
+  // sets highscore for the new level and category
   if (currentUser.highScores.length === 0) {
     currentUser.highScores.push({
       level: selectedLevel,
@@ -220,13 +237,16 @@ function setHighestScore() {
     });
     highestScore = currentScore;
   } else {
+    // checks the user has played same level and category
     let index = currentUser.highScores.findIndex(
       (x) => x.level === selectedLevel && x.category === selectedCategory.name
     );
-    console.log("index", index);
+    // if highscore already exists in level and category?
     if (index > -1) {
-      let highScore = currentUser.highScores[index];
-      if (highScore.score < currentScore) {
+      let highScore = currentUser.highScores[index]; //user currentscore at that level and category
+
+      // check if the currentscore is greater than highscore, update
+      if (currentScore > highScore.score) {
         highScore.score = currentScore;
         highestScore = currentScore;
         currentUser.highScores.splice(index, 1, highScore);
@@ -234,6 +254,7 @@ function setHighestScore() {
         userHighScore.innerText = `Highscore: ${highScore.score}`;
       }
     } else {
+      //else highscore doesnt exist in that level and category
       currentUser.highScores.push({
         level: selectedLevel,
         category: selectedCategory.name,
@@ -246,6 +267,7 @@ function setHighestScore() {
   if (highestScore === 0) return;
 
   userHighScore.innerText = `Highscore: ${highestScore}`;
+  //save highscore into localStorage of current user
   localStorage.setItem(
     currentUser.name.toLowerCase(),
     JSON.stringify(currentUser)
@@ -257,14 +279,17 @@ function setHighestScore() {
 function updateLeaderBoard(highestScore) {
   let key = `${selectedCategory.name} - ${selectedLevel}`;
   let leaderBoard = JSON.parse(localStorage.getItem(key)) || [];
+  // if user high score never existed
   if (leaderBoard.length === 0) {
     leaderBoard.push({
       name: currentUser.name,
       score: highestScore,
     });
   } else {
+    // user highscore exists
     let index = leaderBoard.findIndex((x) => x.name === currentUser.name);
     if (index > -1) {
+      // take the user and find its score set it to the highest score gotten from the sethighest score function
       let highScoreData = leaderBoard[index];
       highScoreData.score = highestScore;
       leaderBoard.splice(index, 1, highScoreData);
